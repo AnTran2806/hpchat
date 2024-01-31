bbcc26b7e094a8c37abdb4b7ba1643f88477feb8
TMA\tduyan.poly <duyan20191999@gmail.com>
2024-01-31 15:12:58 +0700

    Fixed sendfile bug

--- src/server/handleFileTransfer.cpp
+++ src/server/handleFileTransfer.cpp
@@ -10,7 +10,24 @@ void Server::handleFileTransfer(int clientSocket, const string& clientName) {
         cerr << "Error receiving the name to send file to from client " << clientName << endl;
         return;
     }
-    string receiverName = string(buffer, 0, bytesReceived);
+   string receiverName = string(buffer, 0, bytesReceived);
+    //std::cout << "Receiver's name: " << receiverName << std::endl;
+
+    // Send the command "sendfile" to the receiver
+    bool receiverFound = false;
+    for (auto& client : clients) {
+        if ((client.getRoomName() == receiverName || client.getName() == receiverName) && client.getSocket() != clientSocket) {
+            send(client.getSocket(), "sendfile", strlen("sendfile") + 1, 0);
+            receiverFound = true;
+        }
+    }
+
+    // If receiver not found, send a notification to the sender
+    if (!receiverFound) {
+        string notification = "Please enter the correct receiver name. If it is correct, you will receive the message as below:";
+        send(clientSocket, notification.c_str(), notification.size() + 1, 0);
+        return;
+    }
 
     // Receive the file name from the client
     bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
@@ -19,40 +36,18 @@ void Server::handleFileTransfer(int clientSocket, const string& clientName) {
         return;
     }
     string filename = string(buffer, 0, bytesReceived);
-
-    // Send the command "sendfile" to the receiver
-    string lenh = "sendfile";
-    for (auto& client : clients) {
-        bool isRoomNameMatch = client.getRoomName() == receiverName && client.getSocket() != clientSocket;
-        bool isNameMatch = client.getName() == receiverName && client.getSocket() != clientSocket;
-        if (isRoomNameMatch || isNameMatch) {
-            send(client.getSocket(), lenh.c_str(), lenh.size() + 1, 0);
-        }
-    }
-    //if invalid recieved name
-    string lenh1 = "Please enter the correct receiver name. If it is correct, you will receive the message as below:";
-    for (auto& client : clients) {
-        if ( client.getSocket() == clientSocket) {
-            send(client.getSocket(), lenh1.c_str(), lenh1.size() + 1, 0);
-            
-        }
-    }
+    //std::cout << "Received file name: " << filename << std::endl;
 
     // Send the file name to the receiver
     for (auto& client : clients) {
-        bool isRoomNameMatch = client.getRoomName() == receiverName && client.getSocket() != clientSocket;
-        bool isNameMatch = client.getName() == receiverName && client.getSocket() != clientSocket;
-        if (isRoomNameMatch || isNameMatch) {
-
-        // if (client.getRoomName() == receiverName && client.getSocket() != clientSocket || client.getName() == receiverName && client.getSocket() != clientSocket) {
+        if ((client.getRoomName() == receiverName || client.getName() == receiverName) && client.getSocket() != clientSocket) {
             send(client.getSocket(), filename.c_str(), filename.size() + 1, 0);
-            //cout << "Sent received file name to client: " << filename << endl;
         }
     }
 
     // Open the file for writing
-    ofstream fileDuc("abc"+filename, ios::binary | ios::app);
-    if (!fileDuc.is_open()) {
+    ofstream file("abc" + filename, ios::binary |ios::app);
+    if (!file.is_open()) {
         cerr << "Error opening file for writing: " << filename << endl;
         return;
     }
@@ -60,39 +55,33 @@ void Server::handleFileTransfer(int clientSocket, const string& clientName) {
     // Receive and write file data
     while (true) {
         bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-
         if (bytesReceived <= 0) {
-            // Exit the loop when receiving "endsendfile" or if there's an error
             break;
         }
+        if (strcmp(buffer, "exit") == 0) {
+            break;
+        }
+        file.write(buffer, bytesReceived);
 
+        // Forward the received file data to the receiver
         for (auto& client : clients) {
-        bool isRoomNameMatch = client.getRoomName() == receiverName && client.getSocket() != clientSocket;
-        bool isNameMatch = client.getName() == receiverName && client.getSocket() != clientSocket;
-            if (isRoomNameMatch || isNameMatch) {
+            if ((client.getRoomName() == receiverName || client.getName() == receiverName) && client.getSocket() != clientSocket) {
                 send(client.getSocket(), buffer, bytesReceived, 0);
             }
         }
-
-        if (strcmp(buffer,"endsendfile")==0) {
-            for (auto& client : clients) 
-            {
-                bool isRoomNameMatch = client.getRoomName() == receiverName && client.getSocket() != clientSocket;
-                bool isNameMatch = client.getName() == receiverName && client.getSocket() != clientSocket;
-                if (isRoomNameMatch || isNameMatch) {
-                    send(client.getSocket(), "endsendfile", strlen("endsendfile") + 1, 0);
-                }
-            }
-            break; // Add break statement here
-        }
-        fileDuc.write(buffer, bytesReceived);
     }
 
     if (bytesReceived < 0) {
         cerr << "Read Error" << endl;
     }
 
-    fileDuc.close();
-    cout << "successfull to write the file\n";
-    
+    file.close();
+    cout << "Successfully forwarded the file\n";
+
+    // Send "endsendfile" command to the receiver
+    for (auto& client : clients) {
+        if ((client.getRoomName() == receiverName || client.getName() == receiverName) && client.getSocket() != clientSocket) {
+            send(client.getSocket(), "exit", strlen("exit") + 1, 0);
+        }
+    }
 }
